8c4abbed2aeea147ad74c3e64673ab8d415fed59
yalcintereci-sketch <yalcintereci@gmail.com>
2026-07-29 21:54:03 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# Takip edilecek fonlar
+FUNDS = [
+    "PHE",
+    "PBR",
+    "TLY",
+    "TMV"
+]
+
+# Çalışma ayarları
+CHECK_TIME = "08:00"
+
+# E-posta ayarları
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
+
+# GitHub Secrets'tan okunacak bilgiler
+EMAIL_USER = ""
+EMAIL_PASSWORD = ""
+
+# Bildirim gönderilecek e-posta
+RECEIVER_EMAIL = ""
