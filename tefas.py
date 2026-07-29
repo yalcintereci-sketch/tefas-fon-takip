fc6c46d48c591d067235b59c146b859b62525dfb
yalcintereci-sketch <yalcintereci@gmail.com>
2026-07-29 21:58:54 +0300

    Update tefas.py

--- tefas.py
+++ tefas.py
@@ -1,7 +1,22 @@
 import requests
 
-def test():
-    print("TEFAS modülü yüklendi.")
+
+class TefasClient:
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.base_url = "https://www.tefas.gov.tr"
+
+    def check_connection(self):
+        r = self.session.get(self.base_url, timeout=20)
+
+        return {
+            "status": r.status_code,
+            "success": r.ok
+        }
+
 
 if __name__ == "__main__":
-    test()
+    client = TefasClient()
+
+    print(client.check_connection())
